5da59a2711e6be961c3e991829e4119a6622be97
Naman Bhatia <44447629+namanb99@users.noreply.github.com>
2020-10-01 13:33:08 +0530

    Update bubble_sort_demo.cpp

--- src/bubble_sort_demo.cpp
+++ src/bubble_sort_demo.cpp
@@ -1,24 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h> 
-#include <time.h>
+#include<bits/stdc++.h>
+using namespace std;
 
-#include "generic.h"
-#include "bubble_sort.h"
-
-using namespace alg;
-int main (int argc, char const *argv[]) {
-	const int MAX_ELEMENTS = 10;
-	int list[MAX_ELEMENTS];
-	for(int i = 0; i < MAX_ELEMENTS; i++ ){
-		list[i] = rand()%100;
-	}
-	printf("The list before sorting is:\n");
-	printlist(list,MAX_ELEMENTS);
-
-	alg::BubbleSort(list,0,MAX_ELEMENTS-1);
-
-	printf("The list after sorting using bubble-sort algorithm:\n");
-	printlist(list,MAX_ELEMENTS);
+int main () {
 	
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(int i=0;i<n;++i) cin >> a[i];
+	
+	//sorting the input aray using bubble sort
+	for(int i=0;i<n;++i){
+		for(int j=i+1;j<n;++j){
+			if(a[i]>a[j]){
+				swap(a[i],a[j]);
+			}
+		}
+	}
+	
+	for(int i=0;i<n;++i){
+		cout<<a[i]<<" ";
+	}
 	return 0;
 }
